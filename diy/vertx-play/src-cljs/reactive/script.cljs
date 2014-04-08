cdc2eeb25a3aef2a9bc49c4e679b3896d89ef721
buurd <roland@javalia.se>
2014-04-08 21:14:48 +0200

    next

--- diy/vertx-play/src-cljs/reactive/script.cljs
+++ diy/vertx-play/src-cljs/reactive/script.cljs
@@ -2,11 +2,18 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defn widget [data owner]
+(defn header [data owner]
   (reify
     om/IRender
-    (render [this]
-      (dom/h1 nil (:text data)))))
+    (render [_]
+      (dom/div nil
+        (dom/h1 nil "Hello world!")
+        (dom/div nil
+          (dom/ul nil
+            (dom/li nil "Clojure")
+            (dom/li nil "ClojureScript")
+            (dom/li nil "Om")
+            (dom/li nil "Vertx")))))))
 
-(om/root widget {:text "Hello world!"}
+(om/root header {:text "Hello world!"}
   {:target (. js/document (getElementById "my-app"))})
