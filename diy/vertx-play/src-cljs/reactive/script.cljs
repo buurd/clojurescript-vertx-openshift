f62d4cfc244cedec041602768dac22f9980d401b
buurd <roland@javalia.se>
2014-05-06 22:47:24 +0200

    lite närmare

--- diy/vertx-play/src-cljs/reactive/script.cljs
+++ diy/vertx-play/src-cljs/reactive/script.cljs
@@ -1,8 +1,41 @@
 (ns reactive.script
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+  (:require [cljs.reader :as reader]
+            [goog.events :as events]
+            [goog.dom :as gdom]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
 
-(defn header [data owner]
+(def app-state (atom {:count 0 :server 0}))
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+      (send url (meths method) (when data (pr-str data))
+        #js {"Content-Type" "application/edn"}))))
+
+(defn callback [data1 data2]
+  (edn-xhr
+    {:method :post
+     :url (str "/counter/update")
+     :data {:counter (:count data1)}
+     :on-complete
+     (fn [res]
+       (om/update! data2 [:server] (:counter res))
+       )}))
+
+(defn main [data owner]
   (reify
     om/IRender
     (render [_]
@@ -13,7 +46,15 @@
             (dom/li nil "Clojure")
             (dom/li nil "ClojureScript")
             (dom/li nil "Om")
-            (dom/li nil "Vertx")))))))
+            (dom/li nil "Vertx")
+            (dom/button #js {:onClick
+               (fn [e]
+                 (om/transact! data :count inc)
+                 (callback @data data))}
+                 "Klicka mig")
 
-(om/root header {:text "Hello world!"}
+             (dom/div nil (:count data))
+             (dom/div nil (:server data))))))))
+
+(om/root main {:text app-state}
   {:target (. js/document (getElementById "my-app"))})
--- diy/vertx-play/src/reactive/server.clj
+++ diy/vertx-play/src/reactive/server.clj
@@ -1,10 +1,18 @@
 (ns reactive.server
   (:require [vertx.http :as http]
+            [vertx.buffer :as buffer]
             [reactive.page :as page]))
+
+(def counter (atom 0))
+
+(defn update-counter [])
+
+(defn on-body-handler [data]
+  (println data)
+  (swap! counter (fn [n] (+ n (:counter (read-string (buffer/get-string data 0 (.length data))))))))
 
 (defn req-handler [req]
   (println "Got request:" (.uri req))
-  (println "Headers are:" (pr-str (http/headers req)))
   (if (= (.uri req) "/")
     (-> (http/server-response req)
       (http/add-header "Content-Type" "text/html; charset=UTF-8")
@@ -14,11 +22,18 @@
 
   (if (.startsWith (.uri req) "/out")
     (http/send-file (http/server-response req)
-                    (.substring (.uri req) 1 (.length (.uri req))))))
+                    (.substring (.uri req) 1 (.length (.uri req)))))
+
+  (if (.startsWith (.uri req) "/counter/update")
+    (let [new-req (http/on-body req on-body-handler)]
+      (-> (http/server-response new-req)
+          (http/add-header "Content-Type" "text/html; charset=UTF-8")
+          (http/end (str "{:counter " @counter "}"))))))
 
 (-> (http/server)
     (http/on-request req-handler)
-    ;;(http/listen 8080 "localhost"))
+    (http/listen 8080 "localhost"))
 
 
-    (http/listen 8080 "127.10.2.1"))
+    ;;(http/listen 8080 "127.10.2.1"))
+
